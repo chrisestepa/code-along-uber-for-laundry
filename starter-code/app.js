42a550db76bd9468f1a59a0ccd75ebb7210dcbbf
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-15 11:36:54 +0200

    iteraccion 6

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,7 +13,13 @@ const users = require('./routes/users');
 const authRoutes = require('./routes/auth');
 const laundryRoutes = require('./routes/laundry');
 
+// if(process.env.NODE_ENV === 'development'){
+//   require('dotenv').config();
+// }
+
 const app = express();
+
+//mongoose.connect(process.env.MONGO_URI)
 mongoose.connect('mongodb://localhost/uberlaundry');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- starter-code/routes/laundry.js
+++ starter-code/routes/laundry.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
+const LaundryPickup = require('../models/laundry-pickup');
 
 router.use((req, res, next) => {
   if (req.session.currentUser) {
@@ -48,6 +49,40 @@ router.get('/launderers', (req, res, next) => {
   });
 });
 
+router.get('/launderers/:id', (req, res, next) => {
+  const laundererId = req.params.id;
+
+  User.findById(laundererId, (err, theUser) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.render('laundry/launderer-profile', {
+      theLaunderer: theUser
+    });
+  });
+});
+
+
+router.post('/laundry-pickups', (req, res, next) => {
+  const pickupInfo = {
+    pickupDate: req.body.pickupDate,
+    launderer: req.body.laundererId,
+    user: req.session.currentUser._id
+  };
+
+  const thePickup = new LaundryPickup(pickupInfo);
+
+  thePickup.save((err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.redirect('/dashboard');
+  });
+});
 
 
 module.exports = router;
